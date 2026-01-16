9259be7ce0b6296c73db964ae432203fc4b7ab3b
unlomtrois <vadim.kuz02@gmail.com>
2026-01-16 22:15:12 +0300

    implement simple stack-based calculator

--- README.md
+++ README.md
@@ -5,8 +5,23 @@ In this repo I want to experiment with creating a stack-based language. Until no
 Very elementary example:
 
 ```
-    push 2
-    push 3
-    add
-    print # 5
+    2 # 2
+    3 # 2 3
+    4 # 2 3 4
+    add # 2 7
+    mul # 14
+    print
+
 ```
+
+- No ast
+
+# plan
+
+✅ implement basics in `main.zig`
+✅ - stack via arraylist
+✅ - make number opcode, add opcode, print
+✅ - make it print
+
+✅ - make tokenizer
+✅ - that's it for now
--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,195 @@
 const std = @import("std");
 const stacc = @import("stacc");
 
+const Value = union(enum) {
+    number: u32,
+    add: void,
+    sub: void,
+    mul: void,
+    div: void,
+    print: void,
+};
+
+fn tokenizeNumber(src: []const u8) usize {
+    var i: usize = 0;
+    while (i < src.len) {
+        const char: u8 = src[i];
+        if (char < '0' or char > '9') {
+            break;
+        }
+        i += 1;
+    }
+    return i;
+}
+
+fn tokenize(src: []const u8, allocator: std.mem.Allocator) ![]Value {
+    var tokens = std.ArrayList(Value).empty;
+    errdefer tokens.deinit(allocator);
+
+    var i: usize = 0;
+    while (i < src.len) {
+        defer i += 1;
+        const char: u8 = src[i];
+        switch (char) {
+            '0'...'9' => {
+                const len: usize = tokenizeNumber(src[i..]);
+                const number: u32 = try std.fmt.parseInt(u32, src[i .. i + len], 10);
+                i += len;
+                try tokens.append(allocator, .{ .number = number });
+            },
+            '+' => {
+                try tokens.append(allocator, .add);
+            },
+            '-' => {
+                try tokens.append(allocator, .sub);
+            },
+            '*' => {
+                try tokens.append(allocator, .mul);
+            },
+            '/' => {
+                try tokens.append(allocator, .div);
+            },
+            'p' => {
+                try tokens.append(allocator, .print);
+            },
+            ' ' => {
+                continue;
+            },
+            else => {
+                return error.InvalidCharacter;
+            },
+        }
+    }
+    return tokens.toOwnedSlice(allocator);
+}
+
+fn execute(tokens: []Value, allocator: std.mem.Allocator) ![]Value {
+    var stack = std.ArrayList(Value).empty;
+    defer stack.deinit(allocator);
+
+    for (tokens) |item| {
+        switch (item) {
+            .number => {
+                try stack.append(allocator, item);
+            },
+            .add => {
+                std.debug.assert(stack.items.len >= 2);
+                const a = stack.pop().?;
+                std.debug.assert(a == .number);
+                const b = stack.pop().?;
+                std.debug.assert(b == .number);
+                const res = a.number + b.number;
+                try stack.append(allocator, .{ .number = res });
+            },
+            .sub => {
+                std.debug.assert(stack.items.len >= 2);
+                const a = stack.pop().?;
+                std.debug.assert(a == .number);
+                const b = stack.pop().?;
+                std.debug.assert(b == .number);
+                const res = a.number - b.number;
+                try stack.append(allocator, .{ .number = res });
+            },
+            .mul => {
+                std.debug.assert(stack.items.len >= 2);
+                const a = stack.pop().?;
+                std.debug.assert(a == .number);
+                const b = stack.pop().?;
+                std.debug.assert(b == .number);
+                const res = a.number * b.number;
+                try stack.append(allocator, .{ .number = res });
+            },
+            .div => {
+                std.debug.assert(stack.items.len >= 2);
+                const a = stack.pop().?;
+                std.debug.assert(a == .number);
+                const b = stack.pop().?;
+                std.debug.assert(b == .number);
+                if (b.number == 0) {
+                    return error.DivisionByZero;
+                }
+                const res = a.number / b.number;
+                try stack.append(allocator, .{ .number = res });
+            },
+            .print => {
+                std.debug.assert(stack.items.len >= 1);
+                const value = stack.pop().?;
+                switch (value) {
+                    .number => |n| {
+                        std.debug.print("{d}\n", .{n});
+                    },
+                    else => {
+                        std.debug.print("Unknown value: {any}\n", .{value});
+                    },
+                }
+            },
+        }
+    }
+
+    return stack.toOwnedSlice(allocator);
+}
+
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try stacc.bufferedPrint();
-}
+    const allocator = std.heap.page_allocator;
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+    const tokens = try tokenize("2 2 + 2 / p", allocator);
 
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+    _ = execute(tokens, allocator) catch |err| {
+        switch (err) {
+            error.DivisionByZero => {
+                std.debug.print("Error: Division by zero\n", .{});
+            },
+            else => {
+                std.debug.print("Error: {any}\n", .{err});
+            },
         }
     };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+}
+
+test "2 + 2 = 4" {
+    const allocator = std.heap.page_allocator;
+    const tokens = try tokenize("2 2 +", allocator);
+    const result = try execute(tokens, allocator);
+    try std.testing.expect(result[0].number == 4);
+}
+
+test "(2 + 2) / 2 = 2" {
+    const allocator = std.heap.page_allocator;
+    const tokens = try tokenize("2 2 2 + /", allocator);
+    const result = try execute(tokens, allocator);
+    try std.testing.expect(result[0].number == 2);
+}
+
+test "2 * 2 + 2 = 6" {
+    const allocator = std.heap.page_allocator;
+    const tokens = try tokenize("2 2 * 2 +", allocator);
+    const result = try execute(tokens, allocator);
+    try std.testing.expect(result[0].number == 6);
+}
+
+test "2 * (2 + 2) = 8" {
+    const allocator = std.heap.page_allocator;
+    const tokens = try tokenize("2 2 2 + *", allocator);
+    const result = try execute(tokens, allocator);
+    try std.testing.expect(result[0].number == 8);
+}
+
+test "division by zero" {
+    const allocator = std.heap.page_allocator;
+    const tokens = try tokenize("2 2 - 2 /", allocator);
+    const result = execute(tokens, allocator);
+    try std.testing.expectError(error.DivisionByZero, result);
+}
+
+test "tokenize number with multiple digits" {
+    const allocator = std.heap.page_allocator;
+    const tokens = try tokenize("69", allocator);
+    try std.testing.expect(tokens[0].number == 69);
+}
+
+test "69 + 420 = 489" {
+    const allocator = std.heap.page_allocator;
+    const tokens = try tokenize("69 420 +", allocator);
+    const result = try execute(tokens, allocator);
+    try std.testing.expectEqual(@as(u32, 489), result[0].number);
 }
